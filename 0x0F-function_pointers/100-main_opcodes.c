c51b28511ca4c768445957fc36468960a1fada12
Mekonnen <111089049+Mekonnenageze@users.noreply.github.com>
2022-10-14 13:30:39 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,44 +1,35 @@
-#include <stdlib.h>
 #include <stdio.h>
-/**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
- * Return: void
- */
-void print_opcodes(char *a, int n)
-{
-	int i;
 
-	for (i = 0; i < n; i++)
+#include <stdlib.h>
+
+/**
+ * main - Prints the opcodes of this function
+ * @argc: The number of command-line arguments
+ * @argv: The command-line arguments
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int n_bytes, i;
+
+	int (*prog)(int, char **) = &main;
+
+	if (argc == 2)
 	{
-		printf("%.2hhx", a[i]);
-		if (i < n - 1)
-			printf(" ");
+		n_bytes = atoi(argv[1]);
+		if (n_bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+		for (i = 0; i < n_bytes; i++)
+			printf("%02x%c",
+					(unsigned char)*((char *)prog + i), i < n_bytes - 1 ? ' ' : '\n');
 	}
-	printf("\n");
-}
-/**
- * main - prints the opcodes of its own main function
- * @argc: argument count
- * @argv: argument vector
- * Return: O
- */
-int main(int argc, char **argv)
-{
-	int a;
-
-	if (argc != 2)
+	else
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	a = atoi(argv[1]);
-	if (a < 0)
-	{
-		printf("Error\n")
-			exit(2);
-	}
-	print_opcodes((char *)&main, a);
 	return (0);
 }
